3862c1ab728a5ee5aca2400a49bef35be7dea82e
SuuuHyeon <tngutnqls6909@gmail.com>
2024-11-21 10:46:00 +0900

    프로젝트 기초 세팅

---
+++ SERVER/suhyeon_portfolio/src/main/java/itc/cse/suhyeon/suhyeon_portfolio/SuhyeonPortfolioApplication.java
@@ -0,0 +1,13 @@
+package itc.cse.suhyeon.suhyeon_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhyeonPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuhyeonPortfolioApplication.class, args);
+    }
+
+}
---
+++ SERVER/suhyeon_portfolio/src/test/java/itc/cse/suhyeon/suhyeon_portfolio/SuhyeonPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package itc.cse.suhyeon.suhyeon_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuhyeonPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
